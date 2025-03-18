f68877e14b467b2b877323c857933d1bcde95ed2
Abhishek Malkhedkar <abhishekmalkhedkar3@gmail.com>
2025-03-18 11:20:48 +0530

    Added SignUp form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,6 +11,11 @@ const Login = () => {
     const [emailId, setEmailId] = useState("");
     const [password, setpassword] = useState("");
     const [error, setError] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(true);
+
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
@@ -37,12 +42,39 @@ const Login = () => {
 
     }
 
+    const handleSignUp = async () => {
+        try{
+
+            const res = await axios.post(BASE_URL + "/signup", {firstName, lastName, emailId, password}, {withCredentials : true});
+             dispatch(addUser(res.data.data));
+             return navigate("/profile");
+        }catch(err){
+            setError(err?.response?.data || "Something Went Wrong");
+        }
+    }
+
     return (
     <div className="flex justify-center my-10">
         <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">{ isLoginForm ? "Login" : "Sign Up"}</h2>
           <div>
+                { !isLoginForm && <><label className="form-control w-full max-w-xs my-5">
+                    <div className="label">
+                        <span className="label-text">First Name</span>
+                        
+                    </div>
+                <input type="text" value={firstName} className="input input-bordered w-full max-w-xs" onChange={(e) => setFirstName(e.target.value)} />
+                </label>
+                <label className="form-control w-full max-w-xs my-5">
+                    <div className="label">
+                        <span className="label-text">Last Name</span>
+                        
+                    </div>
+                <input type="text" value={lastName} className="input input-bordered w-full max-w-xs" onChange={(e) => setLastName(e.target.value)} />
+                </label>
+                </>
+                }
                 <label className="form-control w-full max-w-xs my-5">
                     <div className="label">
                         <span className="label-text">Email ID</span>
@@ -55,13 +87,15 @@ const Login = () => {
                         <span className="label-text">Password</span>
                         
                     </div>
-                <input type="text" value={password} className="input input-bordered w-full max-w-xs" onChange={(e) => setpassword(e.target.value)} />
+                <input type="password" value={password} className="input input-bordered w-full max-w-xs" onChange={(e) => setpassword(e.target.value)} />
                 </label>
           </div>
           <p className="text-red-500"> {error}</p>
           <div className="card-actions justify-center m-2">
-            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+            <button className="btn btn-primary" onClick={isLoginForm? handleLogin : handleSignUp}>{isLoginForm? "Login" : "Sign Up"}</button>
           </div>
+
+          <p className="m-auto cursor-pointer py-2" onClick={() => setIsLoginForm((value) => !value)}> {isLoginForm? "New User? Signup here" : "Existing User? Login here"}</p>
         </div>
     </div>
     </div>
